3498ec01c96eb62b9f5c6b52daad9d34be865ab9
Rizki Januar I <104696196+rzkjanuarr@users.noreply.github.com>
2023-02-11 08:16:05 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ users }) {
 }
 
 Home.getInitialProps = async () => {
-  const res = await fetch('http://localhost:3000/api/users');
+  const res = await fetch('https://next-apiey.vercel.app/api/users');
   const data = await res.json();
 
   return { users: data };
